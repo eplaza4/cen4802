b8dd912df2f7f51039be0a9553d0418c3cff956d
eplaza4 <143279350+eplaza4@users.noreply.github.com>
2024-01-20 16:18:09 -0500

    Add files via upload

---
+++ FibonacciSequence.java
@@ -0,0 +1,16 @@
+public class FibonacciSequence {
+
+    //Recursive static method to find the nth term in the Fibonacci sequence
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+        //Main method to execute the fibonacci method
+        public static void main(String[] args){
+            int n=10;
+            int result = fibonacci(n);
+            System.out.println("The"+n+"nth term of the Fibonacci sequence is"+result+".");
+        }
+    }
